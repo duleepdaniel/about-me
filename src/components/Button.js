e06aa7183d185e4093fc5febff1f25bc53837772
Duleep <duleep.daniel@gmail.com>
2021-11-29 18:32:23 +0530

    Chore: CSS

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ const ButtonComponent = (props) => {
   return(
     <Button href={props.href} target="_blank" 
             variant={props.variant} size={props.size} className={props.className} style={props.style}>
-      <img src={props.img.src} style={props.img.style} alt={props.img.alt}/> &nbsp; {props.text}
+      <img src={props.img.src} style={props.img.style} alt={props.img.alt}/> {props.text}
     </Button>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -8,28 +8,33 @@ const Main = () => {
     <Container className='context'>
       <Row>
         <Col xs={12} lg={12}>
+          <p className='hello'>ℌ𝔢𝔩𝔩𝔬!</p>
           <p className='context'>
-            Hi! I am Duleep Panthagani a <br />
+            I am Duleep Panthagani a <br />
             <Typed
-            strings={["Web Developer", "Software Engineer", "Gamer "]}
-            typeSpeed={100}
+            strings={["Full Stack Developer", "Software Engineer", "Gamer"]}
+            typeSpeed={90}
             backSpeed={50}
             loop
             style={{color: '#009357'}}
             /> <br/>
             From INDIA. 
-            <p className='sub_context mt-4 mb-3'>You can Find me on <br /></p>
-          </p>
-          <ButtonComponent href='https://github.com/duleepdaniel'
-            variant='dark' size='lg' className='col-lg-2 col-8 mb-2' style={{fontWeight: "bold"}}
-            img={{src: 'github.png', style: {width: 30, alignItems: 'center'}, alt: 'github'}} text='Github'/>{' '}
-          <ButtonComponent href='https://in.linkedin.com/in/duleep-panthagani-a15b53158'
-            variant='outline-secondary' size='lg' className='col-lg-2 col-8 mb-2' style={{fontWeight: "bold"}}
-            img={{src: 'Linkedin.png', style: {width: 30, alignItems: 'center'}, alt: 'linkedin'}} text='LinkedIN'/>
-          <br />
-          <p className='sub_sub_context'>
-            Hit me up if you want to talk about Coding or Video Games over Coffee, of course:)
-          </p>
+            </p>
+            <div className='fixed-bottom'>
+              <p className='sub_context pb-2'>You can Find me on</p>
+              <ButtonComponent href='https://github.com/duleepdaniel'
+                variant='dark' size='lg' className='mb-2' style={{fontWeight: "bold"}}
+                img={{src: 'github.png', style: {width: 30, alignItems: 'center'}, alt: 'github'}} />{' '}
+              <ButtonComponent href='https://in.linkedin.com/in/duleep-panthagani-a15b53158'
+                variant='outline-dark' size='lg' className='mb-2' style={{fontWeight: "bold"}}
+                img={{src: 'Linkedin.png', style: {width: 30, alignItems: 'center'}, alt: 'linkedin'}}/>{' '}
+              <ButtonComponent href='https://wa.me/+919110317308' variant='success' size='lg' className='mb-2' style={{fontWeight: "bold"}}
+                img={{src: 'whatsapp.png', alt: 'whatsapp', style: {width: 30, alignItems: 'center'}}} /> 
+              <br />
+              <p className='sub_sub_context pt-2'>
+                Hit me up if you want to talk about Coding or Video Games. Over Coffee, of course :)
+              </p>
+            </div>
         </Col>
       </Row>
     </Container>
